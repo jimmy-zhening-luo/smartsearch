5745935d4de7875443942a5b13d76f77f0e65cb5
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-10-20 16:59:02 -0700

    v0.2.0: Client created and tested

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,23 @@
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import OpenAiClient from "./client/index.js";
 
-dotenv.config();
-const apiKey: string = process.env.OPENAI_API_KEY ?? "";
-const organization: string = process.env.OPENAI_ORG_ID ?? "";
+function initialize(): OpenAiClient {
+  dotenv.config();
+  const apiKey: string = process.env.OPENAI_API_KEY ?? "";
+  const organization: string = process.env.OPENAI_ORG_ID ?? "";
 
-const openai: OpenAI = new OpenAI({
-  apiKey: apiKey,
-  organization: organization,
-});
+  return new OpenAiClient(new OpenAI({
+    apiKey: apiKey,
+    organization: organization,
+  }));
 
-async function main(): Promise<void> {
-  const chatCompletion: OpenAI.ChatCompletion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: "Say 'hello world'",
-    }
-  ],
-    model: "gpt-4",
-  });
-
-  console.log(chatCompletion.choices);
 }
 
-main();
+async function main(client: OpenAiClient): Promise<void> {
+  const choices: OpenAI.ChatCompletion.Choice[] = await client.complete("Say 'hello world'");
+
+  console.log(choices);
+}
+
+main(initialize());
