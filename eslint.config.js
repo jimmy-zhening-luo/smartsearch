f7fe7bd80a549248fea8ed6c74257ae6afc893f0
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-11-17 19:13:11 -0800

    nit: fix eslint version number

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// v2.0.0
+// v2.0.1
 import tsLint from "@typescript-eslint/eslint-plugin";
 import tsLintParser from "@typescript-eslint/parser";
 import stylistic from "@stylistic/eslint-plugin";
