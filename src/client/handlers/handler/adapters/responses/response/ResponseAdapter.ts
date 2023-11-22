febe163a5c7949913379a189dcf90af89246384e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-11-22 15:24:33 -0800

    Nit: remove extra whitespace

--- src/client/handlers/handler/adapters/responses/response/ResponseAdapter.ts
+++ src/client/handlers/handler/adapters/responses/response/ResponseAdapter.ts
@@ -1,6 +1,6 @@
 import Adapter from "../../adapter/Adapter.js";
 
-export default abstract class ResponseAdapter< ResponsePayload, ResponseOutput>
+export default abstract class ResponseAdapter<ResponsePayload, ResponseOutput>
   extends Adapter<ResponsePayload> {
   readonly payload: ResponsePayload;
   readonly output: ResponseOutput;
