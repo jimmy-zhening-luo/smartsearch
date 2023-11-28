9a552ffe7af50ab7cf46b9aa60914a4b2626db7c
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-11-27 19:58:24 -0800

    Speech integration WIP: Improve client init, add file reader & writer. TBD: Implement speech handler

--- src/client/OpenAIClient.ts
+++ src/client/OpenAIClient.ts
@@ -6,22 +6,34 @@ import ClientDefaults from "./defaults/ClientDefaults.js";
 // Handlers
 import ChatHandler from "./handlers/ChatHandler.js";
 import ModelsHandler from "./handlers/ModelsHandler.js";
+import SpeechHandler from "./handlers/SpeechHandler.js";
 
 // Operators
 import InputDirectory from "./operators/filesystem/directories/InputDirectory.js";
 import OutputDirectory from "./operators/filesystem/directories/OutputDirectory.js";
+import FileReader from "./operators/filesystem/files/FileReader.js";
+import FileWriter from "./operators/filesystem/files/FileWriter.js";
 
 /* Client Implementation */
 export default class OpenAIClient {
   protected readonly openai: OpenAI;
   protected readonly defaults: ClientDefaults;
-  protected readonly directories: {
-    input: InputDirectory;
-    output: OutputDirectory;
+  protected readonly operators: {
+    io: {
+      dir: {
+        input: InputDirectory;
+        output: OutputDirectory;
+      };
+      file: {
+        reader: FileReader;
+        writer: FileWriter;
+      };
+    };
   };
   protected readonly handlers: {
     chat: ChatHandler;
     models: ModelsHandler;
+    speech: SpeechHandler;
   };
 
   constructor(
@@ -32,68 +44,72 @@ export default class OpenAIClient {
       apiKey: string;
       organization?: string;
     },
-    inputDirectory?: InputDirectory | string,
-    outputDirectory?: OutputDirectory | string,
-    overrideDefaults?: ClientDefaults,
+    overrides?: {
+      inputDirectory?: InputDirectory | string;
+      outputDirectory?: OutputDirectory | string;
+      defaults?: ClientDefaults;
+    },
   ) {
     try {
       if (client instanceof OpenAIClient) {
-        this.openai = client.openai;
-        this.defaults = overrideDefaults ?? client.defaults;
-        this.directories = {
-          input: new InputDirectory(
-            inputDirectory ?? client.directories.input,
-            this.defaults.settings.DEFAULT_INPUT_RELATIVE_PATH,
-          ),
-          output: new OutputDirectory(
-            outputDirectory ?? client.directories.output,
-            this.defaults.settings.DEFAULT_OUTPUT_RELATIVE_PATH,
-          ),
-        };
-      }
-      else {
-        this.defaults = overrideDefaults ?? new ClientDefaults();
-        this.directories = {
-          input: new InputDirectory(
-            inputDirectory ?? this.defaults.settings.INPUT_DIRECTORY,
-            this.defaults.settings.DEFAULT_INPUT_RELATIVE_PATH,
-          ),
-          output: new OutputDirectory(
-            outputDirectory ?? this.defaults.settings.OUTPUT_DIRECTORY,
-            this.defaults.settings.DEFAULT_OUTPUT_RELATIVE_PATH,
-          ),
-        };
-
-        if (client instanceof OpenAI)
-          this.openai = client;
+        this.openai = new OpenAI(
+          {
+            apiKey: client.openai.apiKey,
+            organization: client.openai.organization,
+          },
+        );
+        if (overrides === undefined) {
+          this.defaults = client.defaults;
+          this.operators = client.operators;
+          this.handlers = client.handlers;
+        }
         else {
-          if (client === undefined) {
-            const organization: string | undefined = this
-              .defaults.settings
-              .OPENAI_ORG_ID;
-
-            client = organization === undefined
-              ? {
-                  apiKey: this.defaults.settings.OPENAI_API_KEY,
-                }
-              : {
-                  apiKey: this.defaults.settings.OPENAI_API_KEY,
-                  organization: organization,
-                };
-          }
-          if (client.apiKey === "")
-            throw new SyntaxError(`apiKey cannot be an empty string`);
-          else if (client.organization !== undefined && client.organization === "")
-            throw new SyntaxError(`organization cannot be an empty string. If you do not have an organization, omit this member from the input object.`);
-          else
-            this.openai = new OpenAI(client);
+          this.defaults = overrides.defaults ?? client.defaults;
+          this.operators = this._createOperators(
+            this.defaults,
+            overrides.inputDirectory ?? client
+              .operators
+              .io
+              .dir
+              .input,
+            overrides.outputDirectory ?? client
+              .operators
+              .io
+              .dir
+              .output,
+          );
+          this.handlers = this._createHandlers(
+            this.defaults,
+            this.openai,
+          );
         }
       }
-      this.handlers = this._createHandlers(
-        this.openai,
-        this.defaults,
-      );
+      else {
+        this.defaults = overrides?.defaults ?? new ClientDefaults();
+        this.openai = new OpenAI(
+          client instanceof OpenAI
+            ? {
+                apiKey: client.apiKey,
+                organization: client.organization,
+              }
+            : {
+                apiKey: client?.apiKey ?? this.defaults.settings.OPENAI_API_KEY,
+                organization: client?.organization
+               ?? this.defaults.settings.OPENAI_ORG_ID ?? null,
+              },
+        );
+        this.operators = this._createOperators(
+          this.defaults,
+          overrides?.inputDirectory ?? this.defaults.settings.INPUT_DIRECTORY,
+          overrides?.outputDirectory ?? this.defaults.settings.OUTPUT_DIRECTORY,
+        );
+        this.handlers = this._createHandlers(
+          this.defaults,
+          this.openai,
+        );
+      }
     }
+
     catch (e) {
       throw new EvalError(
         `OpenAIClient: ctor: Failed to construct OpenAIClient instance`,
@@ -102,13 +118,47 @@ export default class OpenAIClient {
     }
   }
 
-  private _createHandlers(
-    openai: OpenAI,
+  private _createOperators(
     defaults: ClientDefaults,
-  ): {
-      chat: ChatHandler;
-      models: ModelsHandler;
-    } {
+    inputDirectory: InputDirectory | string,
+    outputDirectory: OutputDirectory | string,
+  ): typeof OpenAIClient.prototype.operators {
+    try {
+      const input: InputDirectory = new InputDirectory(
+        inputDirectory,
+        defaults.settings.DEFAULT_INPUT_RELATIVE_PATH,
+      );
+
+      const output: OutputDirectory = new OutputDirectory(
+        outputDirectory,
+        defaults.settings.DEFAULT_OUTPUT_RELATIVE_PATH,
+      );
+
+      return {
+        io: {
+          dir: {
+            input: new InputDirectory(input),
+            output: new OutputDirectory(output),
+          },
+          file: {
+            reader: new FileReader(input),
+            writer: new FileWriter(output),
+          },
+        },
+      };
+    }
+    catch (e) {
+      throw new EvalError(
+        `OpenAIClient: _createOperators: Failed to create operators`,
+        { cause: e },
+      );
+    }
+  }
+
+  private _createHandlers(
+    defaults: ClientDefaults,
+    openai: OpenAI,
+  ): typeof OpenAIClient.prototype.handlers {
     try {
       return {
         chat: new ChatHandler(
@@ -120,6 +170,14 @@ export default class OpenAIClient {
         models: new ModelsHandler(
           openai,
           undefined,
+        ),
+        speech: new SpeechHandler(
+          openai,
+          {
+            model: defaults.settings.DEFAULT_SPEECH_MODEL,
+            voice: defaults.settings.DEFAULT_SPEECH_VOICE,
+            response_format: defaults.settings.DEFAULT_SPEECH_RESPONSE_FORMAT,
+          },
         ),
       };
     }
