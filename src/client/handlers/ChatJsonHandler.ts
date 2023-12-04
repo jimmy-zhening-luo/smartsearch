1e348ebd384a4521e9ca4e5539e3e664b3047ebd
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-12-03 20:19:36 -0800

    Code Complete: ChatJson/Vision handlers

---
+++ src/client/handlers/ChatJsonHandler.ts
@@ -0,0 +1,86 @@
+import Handler from "./handler/Handler.js";
+import ChatJsonRequestAdapter from "./handler/adapters/requests/ChatJsonRequestAdapter.js";
+import ChatJsonResponseAdapter from "./handler/adapters/responses/ChatJsonResponseAdapter.js";
+import type { ChatModelSupport } from "./types/ChatTypes.js";
+
+export default class ChatJsonHandler extends Handler<
+ChatJsonRequestAdapter,
+typeof ChatJsonRequestAdapter,
+ChatJsonResponseAdapter,
+typeof ChatJsonResponseAdapter,
+[
+  prompt: string,
+  additionalInstructions?: string,
+  temperature?: Extract<ChatJsonRequestAdapter["payload"]["temperature"], number>,
+  maxTokens?: Extract<ChatJsonRequestAdapter["payload"]["max_tokens"], number>,
+  model?: Extract<Extract<ChatModelSupport["json"], ChatJsonRequestAdapter["payload"]["model"]>, string>,
+  seed?: Extract<ChatJsonRequestAdapter["payload"]["seed"], number>,
+],
+{
+  model: Extract<Extract<ChatModelSupport["json"], ChatJsonRequestAdapter["payload"]["model"]>, string>;
+  jsonInstruction: string;
+  temperature: Extract<ChatJsonRequestAdapter["payload"]["temperature"], number>;
+  seed: Extract<ChatJsonRequestAdapter["payload"]["seed"], number>;
+}
+> {
+  constructor(
+    client: typeof ChatJsonHandler.prototype.client,
+    defaults: typeof ChatJsonHandler.prototype.requestInterfaceDefaults,
+  ) {
+    try {
+      super(
+        ChatJsonRequestAdapter,
+        ChatJsonResponseAdapter,
+        client,
+        defaults,
+      );
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatJsonHandler: ctor: Failed to instantiate by calling abstract parent Handler ctor`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected requestInterface(
+    prompt: string,
+    additionalInstructions?: string,
+    temperature?: Extract<ChatJsonRequestAdapter["payload"]["temperature"], number>,
+    maxTokens?: Extract<ChatJsonRequestAdapter["payload"]["max_tokens"], number>,
+    model?: Extract<Extract<ChatModelSupport["json"], ChatJsonRequestAdapter["payload"]["model"]>, string>,
+    seed?: Extract<ChatJsonRequestAdapter["payload"]["seed"], number>,
+  ): ConstructorParameters<typeof ChatJsonRequestAdapter> {
+    try {
+      return [
+        model ?? this.requestInterfaceDefaults.model,
+        seed ?? this.requestInterfaceDefaults.seed,
+        temperature ?? this.requestInterfaceDefaults.temperature,
+        prompt,
+        this.requestInterfaceDefaults.jsonInstruction,
+        additionalInstructions,
+        maxTokens,
+      ];
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatJsonHandler: requestInterface: Failed to return inputs with defaults for request adapter ctor`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected async handle(
+    requestPayload: ChatJsonRequestAdapter["payload"],
+  ): Promise<ChatJsonResponseAdapter["payload"]> {
+    try {
+      return await this.client.chat.completions.create(requestPayload);
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatJsonHandler: handle: Failed to make OpenAI request by passing a request payload to a native client function`,
+        { cause: e },
+      );
+    }
+  }
+}
---
+++ src/client/handlers/ChatVisionHandler.ts
@@ -0,0 +1,86 @@
+import Handler from "./handler/Handler.js";
+import ChatVisionRequestAdapter from "./handler/adapters/requests/ChatVisionRequestAdapter.js";
+import ChatVisionResponseAdapter from "./handler/adapters/responses/ChatVisionResponseAdapter.js";
+import type { ChatModelSupport } from "./types/ChatTypes.js";
+
+export default class ChatVisionHandler extends Handler<
+ChatVisionRequestAdapter,
+typeof ChatVisionRequestAdapter,
+ChatVisionResponseAdapter,
+typeof ChatVisionResponseAdapter,
+[
+  textPrompt: string,
+  images: Array<Buffer | URL>,
+  instructions?: string,
+  temperature?: Extract<ChatVisionRequestAdapter["payload"]["temperature"], number>,
+  maxTokens?: Extract<ChatVisionRequestAdapter["payload"]["max_tokens"], number>,
+  model?: Extract<Extract<ChatModelSupport["vision"], ChatVisionRequestAdapter["payload"]["model"]>, string>,
+  seed?: Extract<ChatVisionRequestAdapter["payload"]["seed"], number>,
+],
+{
+  model: Extract<Extract<ChatModelSupport["vision"], ChatVisionRequestAdapter["payload"]["model"]>, string>;
+  maxTokens: Extract<ChatVisionRequestAdapter["payload"]["max_tokens"], number>;
+}
+> {
+  constructor(
+    client: typeof ChatVisionHandler.prototype.client,
+    defaults: typeof ChatVisionHandler.prototype.requestInterfaceDefaults,
+  ) {
+    try {
+      super(
+        ChatVisionRequestAdapter,
+        ChatVisionResponseAdapter,
+        client,
+        defaults,
+      );
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatVisionHandler: ctor: Failed to instantiate by calling abstract parent Handler ctor`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected requestInterface(
+    textPrompt: string,
+    images: Array<Buffer | URL>,
+    instructions?: string,
+    temperature?: Extract<ChatVisionRequestAdapter["payload"]["temperature"], number>,
+    maxTokens?: Extract<ChatVisionRequestAdapter["payload"]["max_tokens"], number>,
+    model?: Extract<Extract<ChatModelSupport["vision"], ChatVisionRequestAdapter["payload"]["model"]>, string>,
+    seed?: Extract<ChatVisionRequestAdapter["payload"]["seed"], number>,
+  ): ConstructorParameters<typeof ChatVisionRequestAdapter> {
+    try {
+      return [
+        model ?? this.requestInterfaceDefaults.model,
+        maxTokens ?? this.requestInterfaceDefaults.maxTokens,
+        textPrompt,
+        [...images],
+        instructions,
+        temperature,
+        seed,
+      ];
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatVisionHandler: requestInterface: Failed to return inputs with defaults for request adapter ctor`,
+        { cause: e },
+      );
+    }
+  }
+
+  protected async handle(
+    requestPayload: ChatVisionRequestAdapter["payload"],
+  ): Promise<ChatVisionResponseAdapter["payload"]> {
+    try {
+      return await this.client.chat.completions.create(requestPayload);
+    }
+    catch (e) {
+      throw new EvalError(
+        `ChatVisionHandler: handle: Failed to make OpenAI request by passing a request payload to a native client function`,
+        { cause: e },
+      );
+    }
+  }
+}
