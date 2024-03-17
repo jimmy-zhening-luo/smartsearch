732bfb35d7da86ad4faa42c88e47651bc5f7bb98
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-03-17 14:00:47 -0700

    Comment out test cases that don't need to exercise each time; Future: Put behind flatg

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ namespace Program {
       /** TBD */
 
       // Image
-      const imagePrompt: string = "A picture of a cat";
+      const imagePrompt: string = "A family on vacation in Shanghai";
 
       Log.clientResponse(
         "Image",
@@ -33,59 +33,59 @@ namespace Program {
 
       // ImageEdit
       /** OpenAI Bug -- not working */
-      const inputImageEditFilename: string = "cat-square.png";
-      const inputImageEditMaskFilename: string = "mask-transparent-square.png";
-      const imageEditPrompt: string = "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: Show me this cat on vacation in Hawaii";
+      // const inputImageEditFilename: string = "cat-square.png";
+      // const inputImageEditMaskFilename: string = "mask-transparent-square.png";
+      // const imageEditPrompt: string = "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: Show me this cat on vacation in Hawaii";
 
-      const editedImageResponse = await client.imageEdit(
-        inputImageEditFilename,
-        inputImageEditMaskFilename,
-        imageEditPrompt,
-      );
+      // const editedImageResponse = await client.imageEdit(
+      //   inputImageEditFilename,
+      //   inputImageEditMaskFilename,
+      //   imageEditPrompt,
+      // );
 
-      Log.clientResponse(
-        "ImageEdit",
-        `Input: "${inputImageEditFilename}".\nMask: "${inputImageEditMaskFilename}".\nPrompt: "${imageEditPrompt}".\nNumber of images returned: ${editedImageResponse.images.length}`,
-        [
-          ...editedImageResponse.images,
-          ...Array.from(editedImageResponse.prompts.entries())
-            .map(([ image, prompt ]) => `${image}\n${prompt}`),
-        ],
-      );
+      // Log.clientResponse(
+      //   "ImageEdit",
+      //   `Input: "${inputImageEditFilename}".\nMask: "${inputImageEditMaskFilename}".\nPrompt: "${imageEditPrompt}".\nNumber of images returned: ${editedImageResponse.images.length}`,
+      //   [
+      //     ...editedImageResponse.images,
+      //     ...Array.from(editedImageResponse.prompts.entries())
+      //       .map(([ image, prompt ]) => `${image}\n${prompt}`),
+      //   ],
+      // );
 
       // ImageVariation
-      const inputImageVariationFilename: string = "cat-square.png";
+      // const inputImageVariationFilename: string = "cat-square.png";
 
-      const imageVariationResponse = await client.imageVariation(
-        inputImageVariationFilename,
-      );
+      // const imageVariationResponse = await client.imageVariation(
+      //   inputImageVariationFilename,
+      // );
 
-      Log.clientResponse(
-        "ImageVariation",
-        `Input: "${inputImageVariationFilename}".\nNumber of images returned: ${imageVariationResponse.images.length}`,
-        imageVariationResponse.images,
-      );
+      // Log.clientResponse(
+      //   "ImageVariation",
+      //   `Input: "${inputImageVariationFilename}".\nNumber of images returned: ${imageVariationResponse.images.length}`,
+      //   imageVariationResponse.images,
+      // );
 
-      // Models
-      const modelFilter: string = "gpt";
+      // // Models
+      // const modelFilter: string = "gpt";
 
-      Log.clientResponse(
-        "Models",
-        `filter: "${modelFilter}"`,
-        await client.models(modelFilter),
-      );
+      // Log.clientResponse(
+      //   "Models",
+      //   `filter: "${modelFilter}"`,
+      //   await client.models(modelFilter),
+      // );
 
       // Speech
-      const outputSpeechFilename: string = "hello.mp3";
-      const textToSynthesize: string = "Hello world.";
+      // const outputSpeechFilename: string = "hello.mp3";
+      // const textToSynthesize: string = "Hello world.";
 
-      await client.speech(outputSpeechFilename, textToSynthesize);
+      // await client.speech(outputSpeechFilename, textToSynthesize);
 
-      Log.clientResponse(
-        "Speech",
-        `Output: ${outputSpeechFilename}`,
-        `Synthesized text: ${textToSynthesize}`,
-      );
+      // Log.clientResponse(
+      //   "Speech",
+      //   `Output: ${outputSpeechFilename}`,
+      //   `Synthesized text: ${textToSynthesize}`,
+      // );
 
       // Transcribe
       const inputEnglishSpeechFilename: string = "hello-to-transcribe.mp3";
@@ -97,13 +97,13 @@ namespace Program {
       );
 
       // Translate
-      const inputChineseSpeechFilename: string = "nihao-to-translate.mp3";
+      // const inputChineseSpeechFilename: string = "nihao-to-translate.mp3";
 
-      Log.clientResponse(
-        "Translate",
-        `Input: ${inputChineseSpeechFilename}`,
-        await client.translate(inputChineseSpeechFilename),
-      );
+      // Log.clientResponse(
+      //   "Translate",
+      //   `Input: ${inputChineseSpeechFilename}`,
+      //   await client.translate(inputChineseSpeechFilename),
+      // );
     }
     catch (e) {
       console.error(e);
