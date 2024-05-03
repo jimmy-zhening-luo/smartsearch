ed2cf1fdb7129b0fc5791f8b7462b24fe49c7173
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-03 01:32:35 -0700

    update lint v7

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,13 @@
-import stylisticPlugin from "@stylistic/eslint-plugin";
-import jsLintPlugin from "@eslint/js";
-import tsLintPlugin from "@typescript-eslint/eslint-plugin";
-import tsLintParser from "@typescript-eslint/parser";
+import stylistic from "@stylistic/eslint-plugin";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import Configs from "@jimbojet/lint";
 
 export default [
   ...new Configs(
-    stylisticPlugin,
-    jsLintPlugin,
-    tsLintPlugin,
-    tsLintParser,
+    stylistic,
+    ts,
+    tsParser,
     ["eslint.config.js"],
     ["src/**/*.ts"],
   ).configs,
